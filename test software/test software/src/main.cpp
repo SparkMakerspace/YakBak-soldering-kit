e468fc393b4d8214b6db46dc4a4e55ba5192f342
Johnsci <56771439+fsckyou@users.noreply.github.com>
2023-05-04 14:51:49 -0400

    latest changes

--- test software/test software/src/main.cpp
+++ test software/test software/src/main.cpp
@@ -1,9 +1,16 @@
 #include <Arduino.h>
+#include <Preferences.h>
 
-#define SERIAL_EN true
-#define PWM_FREQ 40000
-#define SAMPLE_FREQ 8000
+// Tweak these as desired
+#define SERIAL_EN true    // Disable when we're ready for "production"
+#define PWM_FREQ 40000    // This isn't exactly what it'll be, but that's okay.
+#define SAMPLE_FREQ 8000  // Input and output samples/second
+#define CLOCK_DIVIDER 80  // the APB clock should be running at 80MHz. This clock is used for the timer for the sample interrupt
 
+// don't change these ones
+#define COUNT_PER_SAMPLE APB_CLK_FREQ/CLOCK_DIVIDER/SAMPLE_FREQ // Just a pre-processor calculation to simplify runtime
+
+uint32_t countPerSample = COUNT_PER_SAMPLE;
 uint8_t analogOut = 10;
 uint8_t sayButton = 8;
 uint8_t playButton = 7;
@@ -13,18 +20,25 @@ hw_timer_t * timer = NULL;
 volatile SemaphoreHandle_t timerSemaphore;
 portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
 
-volatile uint32_t isrCounter = 0;
-volatile uint32_t lastIsrAt = 0;
+// used for button state and edge detection
+boolean sayPressed = false;
+boolean wasSayPressed = false;
+boolean sayJustPressed = false;
+boolean playPressed = false;
+boolean wasPlayPressed = false;
+boolean playJustPressed = false;
 
 void ARDUINO_ISR_ATTR onTimer(){
   // Increment the counter and set the time of ISR
   portENTER_CRITICAL_ISR(&timerMux);
-  isrCounter++;
-  lastIsrAt = millis();
+  // TODO: if say was JUST pressed, clear SPIFFS first
+  if (sayJustPressed) {
+    // TODO: clear the SPIFFS file
+  }
+  // TODO: if say button is pressed, read ADC and write to end of SPIFFS
   portEXIT_CRITICAL_ISR(&timerMux);
   // Give a semaphore that we can check in the loop
   xSemaphoreGiveFromISR(timerSemaphore, NULL);
-  // It is safe to use digitalRead/Write here if you want to toggle an output
 }
 
 void setup()
@@ -33,7 +47,7 @@ void setup()
   if (SERIAL_EN)
   {
     Serial.begin();
-    delay(10000); // delay so we have time to connect
+    delay(5000); // delay so we have time to connect
   }
 
   // Pinmodes
@@ -42,10 +56,17 @@ void setup()
   pinMode(playButton, INPUT_PULLUP);
   pinMode(amplifierShutdown, OUTPUT);
 
+  (SERIAL_EN)? Serial.printf("timer count per interrupt firing: %d\n", countPerSample):0;
 
   // turn off the amplifier
   digitalWrite(amplifierShutdown, HIGH);
 
+  Preferences preferences = Preferences();
+  preferences.begin("nvs");
+  (SERIAL_EN)? Serial.printf("Free entries in preferences space: %d\n", preferences.freeEntries()):0;
+
+  // Create semaphore to inform us when the timer has fired
+  timerSemaphore = xSemaphoreCreateBinary();
 
   // Set the PWM frequency, get the actual PWM frequency back
   uint32_t clockFreq = sigmaDeltaSetup(analogOut, 0, PWM_FREQ);
@@ -56,25 +77,36 @@ void setup()
   }
   else
   {
-    (SERIAL_EN)? Serial.printf("PWM clock freqncy: %d\n", clockFreq):0;
+    (SERIAL_EN)? Serial.printf("PWM clock frequency: %d\n", clockFreq):0;
   }
   // No output yet
   sigmaDeltaWrite(0, 0);
 
-
   // Setup timer at the sample rate
-  timer = timerBegin(0,80,true);
+  timer = timerBegin(0, CLOCK_DIVIDER, true);
   // attach an ISR
-  timerAttachInterrupt(timer,&onTimer,true);
-  // enable "alarms" to trigger the ISR and reload the timer
-
+  timerAttachInterrupt(timer, &onTimer, true);  
+  // Set alarm to call onTimer function every second (value in microseconds).
+  // Repeat the alarm (third parameter)
+  timerAlarmWrite(timer, countPerSample, true);
+  // Start an alarm to enable interrupts
+  timerAlarmEnable(timer);
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
-  // check for button push
-  // if say is being held down, record mic value to SPIFFS every sample rate
-  // if a high->low play transition occurs, read every sample in SPIFFS to the 
-  // sigmaDeltaWrite function every sample rate
+  if (xSemaphoreTake(timerSemaphore, 0) == pdTRUE){}
+  // TODO: check for button push
+  portENTER_CRITICAL(&timerMux);
+    wasSayPressed = sayPressed;
+    sayPressed = !digitalRead(sayButton);
+    sayJustPressed = !wasSayPressed && sayPressed;
+    wasPlayPressed = playPressed;
+    playPressed = !digitalRead(playButton);
+    sayJustPressed = !wasPlayPressed && playPressed;
+  portEXIT_CRITICAL(&timerMux);
+  // TODO: if say is being held down, record mic value to SPIFFS every sample rate
+  // TODO: if a high->low play transition occurs, read every sample in SPIFFS to the 
+  //       sigmaDeltaWrite function every sample rate
 }
